ea68d1fb02b83920845ce240b9a17f7c903371ce
sirjani05 <keithjani05@gmail.com>
2026-08-27 21:32:37 +0200

    create resume section

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 /* menu */
-const navMenu = document.getElementById('nav-menu'), navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle");
 
 /* menu show - hide */
-navToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('show-menu');
-  navToggle.classList.toggle('animate-toggle');
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show-menu");
+  navToggle.classList.toggle("animate-toggle");
 });
 
 /* remove menu mobile */
@@ -14,11 +15,11 @@ navToggle.addEventListener('click', () => {
 /* scroll sections active link */
 
 /* services swiper */
-var servicesSwiper = new Swiper('.services-swiper', {
+var servicesSwiper = new Swiper(".services-swiper", {
   spaceBetween: 32,
 
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 
   breakpoints: {
@@ -28,79 +29,103 @@ var servicesSwiper = new Swiper('.services-swiper', {
     1208: {
       slidesPerView: 3,
     },
-
   },
 
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
 });
 
 /* mixitup filter portfolio */
-var mixer = mixitup('.work-container', {
-    selectors: {
-        target: '.mix'
-    },
-    animation: {
-        duration: 300
-    }
+var mixer = mixitup(".work-container", {
+  selectors: {
+    target: ".mix",
+  },
+  animation: {
+    duration: 300,
+  },
 });
 
 /* active work */
-const linkWork = document.querySelectorAll('.work-item');
+const linkWork = document.querySelectorAll(".work-item");
 
 function activeWork() {
   linkWork.forEach((a) => {
-    a.classList.remove('active-work');
+    a.classList.remove("active-work");
   });
 
-  this.classList.add('active-work');
+  this.classList.add("active-work");
 }
 
-linkWork.forEach((a) => a.addEventListener('click', activeWork));
+linkWork.forEach((a) => a.addEventListener("click", activeWork));
 
 /* resume */
+const accordianItems = document.querySelectorAll(".resume-item");
+
+accordianItems.forEach((item) => {
+  const header = item.querySelector(".resume-header"),
+    content = item.querySelector(".resume-content"),
+    icon = item.querySelector(".resume-icon i");
+
+  header.addEventListener("click", () => {
+    const isOpen = item.classList.toggle("accordian-open");
+
+    content.style.height = isOpen ? content.scrollHeight + "px" : "0";
+    icon.className = isOpen ? "ri-subtract-line" : "ri-add-line";
+
+    accordianItems.forEach((otherItem) => {
+      if (
+        otherItem !== item &&
+        otherItem.classList.contains("accordian-open")
+      ) {
+        otherItem.querySelector(".resume-content").style.height = "0";
+        otherItem.querySelector(".resume-icon i").classList = "ri-add-line";
+        otherItem.classList.remove("accordian-open");
+      }
+    });
+  });
+});
 
 /* testimonials swiper */
 
 /* email js */
 
 /* style switcher */
-const styleSwitcher = document.getElementById('style-switcher'),
-  switcherToggle = document.getElementById('switcher-toggle'),
-  switcherClose = document.getElementById('switcher-close');
+const styleSwitcher = document.getElementById("style-switcher"),
+  switcherToggle = document.getElementById("switcher-toggle"),
+  switcherClose = document.getElementById("switcher-close");
 
 /* switcher show */
-switcherToggle.addEventListener('click', () => {
-  styleSwitcher.classList.add('show-switcher');
+switcherToggle.addEventListener("click", () => {
+  styleSwitcher.classList.add("show-switcher");
 });
 
 /* switcher hidden */
-switcherClose.addEventListener('click', () => {
-  styleSwitcher.classList.remove('show-switcher');
+switcherClose.addEventListener("click", () => {
+  styleSwitcher.classList.remove("show-switcher");
 });
 
 /* theme colors */
-const colors = document.querySelectorAll('.style-switcher-color');
+const colors = document.querySelectorAll(".style-switcher-color");
 
 colors.forEach((color) => {
   color.onclick = () => {
-    const activeColor = color.style.getPropertyValue('--hue');
+    const activeColor = color.style.getPropertyValue("--hue");
 
-    colors.forEach((c) => c.classList.remove('active-color'));
-    color.classList.add('active-color');
+    colors.forEach((c) => c.classList.remove("active-color"));
+    color.classList.add("active-color");
 
-    document.documentElement.style.setProperty('--hue', activeColor);
+    document.documentElement.style.setProperty("--hue", activeColor);
   };
 });
 
 /* light/dark mode */
-let currentTheme = 'light';
+let currentTheme = "light";
 document.body.className = currentTheme;
 
 document.querySelectorAll('input[name="body-theme"]').forEach((input) => {
-  input.addEventListener('change', () => {
+  input.addEventListener("change", () => {
     currentTheme = input.value;
-    document.body.className = currentTheme
+    document.body.className = currentTheme;
   });
 });
